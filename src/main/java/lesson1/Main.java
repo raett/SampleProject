24f0d08831f2075d7e5a726b54af6251e015a50a
Anatolii Marandiuc <shamaneeel@mail.ru>
2019-09-21 17:07:38 +0300

    гипотинуза

--- src/main/java/lesson1/Main.java
+++ src/main/java/lesson1/Main.java
@@ -1,22 +1,21 @@
 package lesson1;
 
+import java.util.Scanner;
+
 public class Main {
 
+
+
     public static void main(String[] args) {
-        System.out.println("Alloha world");
-        int number = 0; //4b
-        float floatNumber = 5.1234567891234567f;//4b
-        long longNumber = 546457457547L;//8b
-        boolean booleanFlag = false;//
-        byte byteNumber = 4;
-        double doubleNumber = 5.1234567891234567;
-        char charSymbol = 'X';
-        short shortNumber = 436;//2b
-        System.out.println("Alloha from Danila");
-        System.out.println("Alloha from Danila");
-        System.out.println("Alloha from Danila");
-        System.out.println("Alloha from Danila");
-        System.out.println("Alloha from Danila");
-	little change from PC
+        Scanner sc = new Scanner(System.in);
+        float numberB = sc.nextInt();
+        float numberA = sc.nextInt();
+        double numberC;
+        numberC=Math.sqrt(numberA*numberA+numberB*numberB);
+        System.out.println(numberC);
+
     }
+
+
+
 }
