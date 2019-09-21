7aedcdda8057293c2e119604345681e7129017f2
Анатолий Марандюк <shamaneeel@mail.ru>
2019-09-21 13:31:19 +0300

    all types Java

---
+++ src/main/java/lesson1/Main.java
@@ -0,0 +1,19 @@
+package lesson1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Alloha world");
+        int number = 0; //4b
+        float floatNumber = 5.1234567891234567f;//4b
+        long longNumber = 546457457547L;//8b
+        boolean booleanFlag = false;//
+        byte byteNumber = 4;
+        double doubleNumber = 5.1234567891234567;
+        char charSymbol = 'X';
+        short shortNumber = 436;//2b
+
+
+
+    }
+}
