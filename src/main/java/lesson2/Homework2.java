45fb59ff4634f090aecf548acc6118f0eeed3818
Danila <89854821428@yandex.ru>
2019-10-03 15:22:53 +0300

    2 Homework

---
+++ src/main/java/lesson2/Homework2.java
@@ -0,0 +1,14 @@
+package lesson2;
+
+import java.util.Scanner;
+
+public class Homework2 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String str=sc.nextLine();
+        if (str.startsWith("Здравствуйте")){
+            System.out.println("Вежливое");
+        }
+
+    }
+}
